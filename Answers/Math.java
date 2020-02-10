c39ec46f8715cb392662119bbf121bdba4c1c680
Nicholas Edwards <nicholas6598@gmail.com>
2020-02-10 13:17:41 -0500

    Chapter 1

---
+++ Answers/Math.java
@@ -0,0 +1,8 @@
+public class Math{
+   public static void main(String [] args){
+      System.out.println("2+2=4");
+      System.out.println("2-2=0");
+      System.out.println("2*2=4");
+      System.out.println("2/2=1");
+   }
+}
---
+++ Answers/MyInfo.java
@@ -0,0 +1,8 @@
+public class MyInfo{
+   public static void main(String [] args){
+      System.out.println("Hello, my name is Nick.");
+      
+      System.out.print("I am a computer scientist.  ");
+      System.out.print("I love to code!");
+   }
+}
